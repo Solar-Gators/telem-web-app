28d27efaad62ec35ca02db388fad396593d1720c
Jack Schedel <jackschedel@gmail.com>
2025-07-01 17:21:48 -0400

    use map fn

--- lib/db-utils.ts
+++ lib/db-utils.ts
@@ -1,6 +1,6 @@
 'use server'
 import { neon } from "@neondatabase/serverless";
-import { TelemetryData } from "@/lib/types";
+import { mapTelemetryData } from "./telemetry-utils";
 
 export async function fetchLatestTelemetryData() {
   try {
@@ -18,54 +18,9 @@ export async function fetchLatestTelemetryData() {
       throw new Error("No telemetry data found");
     }
 
-    const data = result[0];
-
-    const telemetryData: TelemetryData = {
-      gps: {
-        rx_time: data.gps_rx_time,
-        longitude: data.gps_longitude,
-        latitude: data.gps_latitude,
-        speed: data.gps_speed,
-        num_sats: data.gps_num_sats,
-      },
-      battery: {
-        sup_bat_v: data.battery_sup_bat_v,
-        main_bat_v: data.battery_main_bat_v,
-        main_bat_c: data.battery_main_bat_c,
-        low_cell_v: data.battery_low_cell_v,
-        high_cell_v: data.battery_high_cell_v,
-        high_cell_t: data.battery_high_cell_t,
-        cell_idx_low_v: data.battery_cell_idx_low_v,
-        cell_idx_high_t: data.battery_cell_idx_high_t,
-      },
-      mppt1: {
-        input_v: data.mppt1_input_v,
-        input_c: data.mppt1_input_c,
-        output_v: data.mppt1_output_v,
-        output_c: data.mppt1_output_c,
-      },
-      mppt2: {
-        input_v: data.mppt2_input_v,
-        input_c: data.mppt2_input_c,
-        output_v: data.mppt2_output_v,
-        output_c: data.mppt2_output_c,
-      },
-      mppt3: {
-        input_v: data.mppt3_input_v,
-        input_c: data.mppt3_input_c,
-        output_v: data.mppt3_output_v,
-        output_c: data.mppt3_output_c,
-      },
-      mitsuba: {
-        voltage: data.mitsuba_voltage,
-        current: data.mitsuba_current,
-      },
-    };
-
-    return telemetryData;
+    return mapTelemetryData(result[0]);
   } catch (error) {
     console.error("Error fetching latest telemetry data:", error);
     return null;
   }
 }
-
